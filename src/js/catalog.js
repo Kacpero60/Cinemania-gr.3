a318296eb6b09f6176aa5ecd7c82df799972b372
Kacpero60 <kapiw2002@gmail.com>
2024-10-19 12:18:55 +0200

    poprawiony js katalogu

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -9,7 +9,7 @@ let keyWord = '';
 let selectedCountry = '';
 let selectedYear = '';
 
-//funkcja, co daje kraje do wyboru
+// funkcja do wyboru krajów
 async function selector() {
     const countrySelect = document.getElementById('countrySelect');
 
@@ -17,22 +17,22 @@ async function selector() {
         const response = await fetch(`https://restcountries.com/v3.1/all`);
         const countries = await response.json();
 
-        for (let i = 0; i < countries.length; i++) { 
+        for (let i = 0; i < countries.length; i++) {
             const country = countries[i];
             const option = document.createElement('option');
             option.value = country.cca2;
             option.textContent = country.name.common;
             countrySelect.appendChild(option);
         }
-    } catch(error) {
-        console.error('error fetching countries');
+    } catch (error) {
+        console.error('Błąd podczas pobierania krajów:', error);
     }
 }
 
-//funkcja co daje lata do wyboru
+// funkcja do wyboru lat
 async function populateYears() {
     const yearSelect = document.getElementById('yearSelect');
-    const currentYear = new Date().getFullYear(); 
+    const currentYear = new Date().getFullYear();
 
     for (let year = currentYear; year >= 1980; year--) {
         const option = document.createElement('option');
@@ -42,102 +42,80 @@ async function populateYears() {
     }
 }
 
-//renderowanie przycisków
+// renderowanie przycisków paginacji
 async function renderBtn() {
     const paginationBtn = document.getElementById('pagination');
     paginationBtn.innerHTML = '';
 
-   //przycisk do cofania '<'
-   if (currentPage >= 1) {
+    // przycisk do cofania
+    if (currentPage >= 1) {
         const prevBtn = document.createElement('button');
         prevBtn.textContent = '<';
         prevBtn.classList.add('prevnext-btn');
 
-        
         prevBtn.addEventListener('click', () => {
             currentPage--;
-            
-            if (keyWord) {
-                searchMovies(keyWord, currentPage)
-            } else {
-                popularMovies(currentPage);
-            }
+            keyWord ? searchMovies(keyWord, currentPage) : popularMovies(currentPage);
             renderBtn();
         });
         paginationBtn.appendChild(prevBtn);
     }
-    
-    //wyświetlenie pierwszych trzech stron
+
+    // wyświetlanie pierwszych trzech stron
     for (let i = 1; i <= Math.min(3, totalPages); i++) {
         const btn = document.createElement('button');
         btn.textContent = i;
         btn.classList.add('pagination-btn');
-        
-        if( i === currentPage) {
+
+        if (i === currentPage) {
             btn.classList.add('active');
         }
 
         btn.addEventListener('click', () => {
             currentPage = i;
-
-            if (keyWord) {
-                searchMovies(keyWord, currentPage);
-            } else {
-                popularMovies(currentPage);
-            }
+            keyWord ? searchMovies(keyWord, currentPage) : popularMovies(currentPage);
             renderBtn();
         });
         paginationBtn.appendChild(btn);
-   }
+    }
 
-   
-   //kropki, gdy jest wiecej niz 3 strony
-   if (totalPages > 3) {
-       const dots =document.createElement('div');
-       dots.textContent = '...';
-       paginationBtn.appendChild(dots);
-       dots.classList.add('dots');
-       
-       //wyswietlenie 24 strony
-       const lastBtn = document.createElement('button');
-       lastBtn.textContent = 24;
-       lastBtn.classList.add('pagination-btn');
-       
-       lastBtn.addEventListener('click', () => {
-           currentPage = 24;
-           
-           if (keyWord) {
-               searchMovies(keyWord, currentPage);
-            } else {
-                popularMovies(currentPage);
-            }
+    // kropki, jeśli jest więcej niż 3 strony
+    if (totalPages > 3) {
+        const dots = document.createElement('div');
+        dots.textContent = '...';
+        paginationBtn.appendChild(dots);
+        dots.classList.add('dots');
+
+        // wyświetlenie ostatniej strony
+        const lastBtn = document.createElement('button');
+        lastBtn.textContent = 24;
+        lastBtn.classList.add('pagination-btn');
+
+        lastBtn.addEventListener('click', () => {
+            currentPage = 24;
+            keyWord ? searchMovies(keyWord, currentPage) : popularMovies(currentPage);
             renderBtn();
         });
         paginationBtn.appendChild(lastBtn);
-   }
+    }
 
-   //przycisk do nastepnej strony '>'
-   if (currentPage < totalPages) {
-    const nextBtn = document.createElement('button');
-    nextBtn.textContent = '>';
-    nextBtn.classList.add('prevnext-btn');
+    // przycisk do następnej strony
+    if (currentPage < totalPages) {
+        const nextBtn = document.createElement('button');
+        nextBtn.textContent = '>';
+        nextBtn.classList.add('prevnext-btn');
 
-    nextBtn.addEventListener('click', () => {
-        currentPage++;
-
-        if (keyWord) {
-            searchMovies(keyWord, currentPage)
-        } else {
-            popularMovies(currentPage);
-        }
-        renderBtn();
-    });
-    paginationBtn.appendChild(nextBtn);
-}
+        nextBtn.addEventListener('click', () => {
+            currentPage++;
+            keyWord ? searchMovies(keyWord, currentPage) : popularMovies(currentPage);
+            renderBtn();
+        });
+        paginationBtn.appendChild(nextBtn);
+    }
 }
 
-//funkcja do pobierania popularnych filmów, jest wyświetlana po załadowaniu strony
-async function popularMovies(page = 1, selectedCountry = '', selectedYear = '') {
+// pobieranie popularnych filmów
+async function popularMovies(page = 1) {
     const regionParams = selectedCountry ? `&region=${selectedCountry}` : '';
     const yearParams = selectedYear ? `&primary_release_year=${selectedYear}` : '';
     const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=en-US&include_adult=false&page=${page}${regionParams}${yearParams}`;
@@ -145,45 +123,43 @@ async function popularMovies(page = 1, selectedCountry = '', selectedYear = '') 
     try {
         const response = await fetch(apiUrl);
         if (!response.ok) {
-            throw new Error('response was not ok')
+            throw new Error('response was not ok');
         }
         const data = await response.json();
-
+        totalPages = data.total_pages;
         gallery.innerHTML = '';
 
         if (data.results.length > 0) {
-            for (const movie of data.results) {
+            data.results.forEach(movie => {
                 const movieEl = document.createElement('div');
                 movieEl.className = 'movie';
 
                 const imgEl = document.createElement('img');
-                imgEl.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`; 
-                
-                const titleEl = document.createElement('p')
-                titleEl.textContent = movie.title
+                imgEl.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+                const titleEl = document.createElement('p');
+                titleEl.textContent = movie.title;
                 titleEl.className = 'movie-title';
 
                 movieEl.appendChild(imgEl);
-                gallery.appendChild(movieEl);
                 movieEl.appendChild(titleEl);
+                gallery.appendChild(movieEl);
 
                 movieEl.addEventListener("click", () => {
-                    openPopUp(movie, apikey); // Przekazanie apikey
+                    openPopUp(movie, apikey);
                 });
-            }
+            });
         } else {
-            gallery.textContent = 'OOPS...We are very sorry! You dont have any results matching your search.';
+            gallery.textContent = 'OOPS... Brak wyników pasujących do wyszukiwania.';
         }
+
+        renderBtn();
     } catch (error) {
-=======
-totalPages = data.total_pages;
-renderBtn();
-} catch (error) {
-        console.error('error fetching movies:', error);
+        console.error('Błąd podczas pobierania filmów:', error);
     }
 }
 
-//funkcja co wyszukuje wszystkie filmy po wpisaniu keyword'a
+// wyszukiwanie filmów po wpisaniu frazy
 async function searchMovies(keyWord, page = 1) {
     const yearParams = selectedYear ? `&primary_release_year=${selectedYear}` : '';
     const regionParams = selectedCountry ? `&region=${selectedCountry}` : '';
@@ -195,7 +171,7 @@ async function searchMovies(keyWord, page = 1) {
             throw new Error('response was not ok');
         }
         const data = await response.json();
-
+        totalPages = data.total_pages;
         gallery.innerHTML = '';
 
         if (data.results.length > 0) {
@@ -203,43 +179,31 @@ async function searchMovies(keyWord, page = 1) {
                 const movieEl = document.createElement('div');
                 movieEl.className = 'movie';
 
-                //pobieranie obrazów dla danego filmu
-                const imageResponse = await fetch(`https://api.themoviedb.org/3/movie/${movie.id}/images?api_key=${apikey}`);
-                const imageData = await imageResponse.json();
+                const imgEl = document.createElement('img');
+                imgEl.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
-                //sprawdzanie czy są dostępne postery
-                if (imageData.posters.length > 0) {
-                    const imgEl = document.createElement('img');
-                    imgEl.src = `https://image.tmdb.org/t/p/w500${imageData.posters[0].file_path}`; 
-                    movieEl.appendChild(imgEl);
-                }
-
-                //tytuł filmu
                 const titleEl = document.createElement('p');
                 titleEl.textContent = movie.title;
                 titleEl.className = 'movie-title';
 
-
-                gallery.appendChild(movieEl);
+                movieEl.appendChild(imgEl);
                 movieEl.appendChild(titleEl);
+                gallery.appendChild(movieEl);
             }
         } else {
-            gallery.textContent = 'OOPS...We are very sorry! You dont have any results matching your search.';
+            gallery.textContent = 'OOPS... Brak wyników pasujących do wyszukiwania.';
         }
+
+        renderBtn();
     } catch (error) {
-=======
-totalPages = data.total_pages;
-renderBtn();
-} catch (error) {
-        console.error('error fetching movies:', error);
+        console.error('Błąd podczas wyszukiwania filmów:', error);
     }
 }
 
-//wyszukiwanie przycisku
+// wyszukiwanie po kliknięciu przycisku
 document.getElementById('searchButton').addEventListener('click', function(event) {
     event.preventDefault();
 
-    //wartość inputa
     keyWord = document.getElementById('searchInput').value;
     selectedYear = document.getElementById('yearSelect').value;
     selectedCountry = document.getElementById('countrySelect').value;
@@ -248,12 +212,11 @@ document.getElementById('searchButton').addEventListener('click', function(event
         return;
     }
 
-    //wywołanie searchMovies
     currentPage = 1;
     searchMovies(keyWord);
 });
 
-//wywołanie popularMovies po załadowaniu strony
+// ładowanie popularnych filmów po załadowaniu strony
 document.addEventListener("DOMContentLoaded", () => {
     populateYears();
     selector();
