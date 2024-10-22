703a3075b9ecf9c8ab4b2ca60c42d59558a42ce1
Kacpero60 <150844633+Kacpero60@users.noreply.github.com>
2024-10-22 10:48:14 +0000

    Deploying to gh-pages from @ Kacpero60/Cinemania-gr.3@14e792a88757cad33320a82126aeed535b77b377 🚀

--- index.20bcd4f2.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".more-details"),t=document.getElementById("background-modal"),l=document.getElementById("closed-modal-details"),d=document.querySelector(".photo-modal-details");e.addEventListener("click",(e=>{e.preventDefault(),t.style.display="flex";const l=document.createElement("img");l.src="",l.className="image-deteils",console.log(l.src),d.append(l)})),l.addEventListener("click",(e=>{e.preventDefault(),t.style.display="none",d.innerHTML=""}));
-//# sourceMappingURL=index.20bcd4f2.js.map
